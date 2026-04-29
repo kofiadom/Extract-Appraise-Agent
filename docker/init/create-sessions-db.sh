0d1983d38aebd2dbe98998534a640102f1293273
REST Extractor <dev@rest-extractor.local>
2026-04-29 23:27:36 +0000

    fix postgres startup: fresh volume name, healthcheck start_period, idempotent init script

--- docker/init/create-sessions-db.sh
+++ docker/init/create-sessions-db.sh
@@ -4,8 +4,10 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-  CREATE DATABASE agno_sessions;
+  SELECT 'CREATE DATABASE agno_sessions'
+  WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'agno_sessions')\gexec
+
   GRANT ALL PRIVILEGES ON DATABASE agno_sessions TO $POSTGRES_USER;
 EOSQL
 
-echo "agno_sessions database created."
+echo "agno_sessions database ready."
